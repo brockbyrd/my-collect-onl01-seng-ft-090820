916fa19bfe16210287e9b87326f7ee2f1b404e16
Brock Byrd <brockbyrdd@gmail.com>
2020-09-11 14:38:24 +0000

    Automatically backed up by Learn

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -2,7 +2,7 @@ def my_collect(languages)
   i = 0
   new_array = []
   while i < languages.length
-    new_array << yeild(languages[i])
+    new_array << yield(languages[i])
     i += 1
   end
   new_array
